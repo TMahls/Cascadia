eb3843ce014b2c512dc6417ef3848d45327f2294
tmahlman <tmahlman@mathworks.com>
2025-01-14 19:00:13 -0800

    Fox A, B, and C done. Fox D is complex!

--- Game Core/Scoring/FoxRules.m
+++ Game Core/Scoring/FoxRules.m
@@ -1,30 +1,84 @@
 classdef FoxRules < WildlifeScoreObjective
-    
+
     properties
-        
+
     end
-    
+
     methods
         function obj = FoxRules()
 
         end
 
         function score = ruleAScore(obj, environment)
-            score = 0;
+            score = pointsForGroups(obj, environment, AnimalEnum.Fox, 'foxAShape');
         end
 
         function score = ruleBScore(obj, environment)
-            score = 0;
+            score = pointsForGroups(obj, environment, AnimalEnum.Fox, 'foxBShape');
         end
 
         function score = ruleCScore(obj, environment)
-            score = 0;
+            score = pointsForGroups(obj, environment, AnimalEnum.Fox, 'foxCShape');
         end
 
         function score = ruleDScore(obj, environment)
-            score = 0;
+            score = pointsForGroups(obj, environment, AnimalEnum.Fox, 'foxDShape');
         end
-        
+
     end
+
+    methods (Access = private)
+        function groupScore = foxAShape(obj, environment, groupCoords)
+            groupScore = 0;
+            for i = 1:size(groupCoords,1) % For each fox in the group
+                tile = tileAtCoords(environment, groupCoords(i,:));
+
+                neighborAnimals = getAdjacentAnimals(obj, environment, tile);
+
+                groupScore = groupScore + nnz(neighborAnimals);
+            end
+        end
+
+        function groupScore = foxBShape(obj, environment, groupCoords)
+            groupScore = 0;
+            for i = 1:size(groupCoords,1) % For each fox in the group
+                tile = tileAtCoords(environment, groupCoords(i,:));
+
+                neighborAnimals = getAdjacentAnimals(obj, environment, tile);
+
+                pairIdx = (neighborAnimals == 2); % Number of pairs 
+                
+                % Remove fox pairs 
+                pairIdx(AnimalEnum.Fox + 1) = 0;
+
+                if any(neighborAnimals == 2)
+                    groupScore = groupScore + (2 * nnz(pairIdx) + 1);
+                end
+            end
+        end
+
+        function groupScore = foxCShape(obj, environment, groupCoords)
+            groupScore = 0;
+            for i = 1:size(groupCoords,1) % For each fox in the group
+                tile = tileAtCoords(environment, groupCoords(i,:));
+
+                neighborAnimals = getAdjacentAnimals(obj, environment, tile);
+                [maxAnimals, idx] = max(neighborAnimals);
+                if idx ~= (AnimalEnum.Fox + 1) % Don't include foxes
+                    groupScore = groupScore + maxAnimals;
+                end
+            end
+        end
+
+        function groupScore = foxDShape(obj, environment, groupCoords)
+            % This one has complicated edge-cases. Luckily, it's been
+            % answered by Randy! https://boardgamegeek.com/thread/2817617/fox-card-d-rule-clarification
+            groupScore = 0;
+
+        end
+
+
+    end
+
 end
 
--- Game Core/Scoring/WildlifeScoreObjective.m
+++ Game Core/Scoring/WildlifeScoreObjective.m
@@ -37,6 +37,54 @@ classdef WildlifeScoreObjective
             end
         end
 
+        function totalPoints = pointsForGroups(~, env, animal, shapeFunc)
+            % Iterates through all groups in an environment, and returns
+            % the total number of points for a particular rule. 'shapeFunc'
+            % is the name of a function that returns the number of
+            % points a group gets based on its coordinates
+
+            allTiles = [env.StarterHabitatTile, env.HabitatTiles];
+            totalPoints = 0; overallCoordList = []; 
+            for i = 1:length(allTiles)
+                currTile = allTiles(i);
+
+                if ~isempty(overallCoordList)
+                    tileNotInList = ~ismember(currTile.Coordinate,overallCoordList,"rows");
+                else
+                    tileNotInList = true;
+                end
+               
+                if ~isempty(currTile.WildlifeToken.Animal) && ...
+                        (currTile.WildlifeToken.Animal == animal) && tileNotInList
+                    groupCoords = recursiveGetAnimalGroupCoords(env, currTile, animal, currTile.Coordinate);
+                    
+                    totalPoints = totalPoints + feval(shapeFunc, env, groupCoords);
+
+                    overallCoordList = [overallCoordList; groupCoordList];                
+                end
+            end
+        end
+
+        function animalsFound = getAdjacentAnimals(~, environment, tile)
+            % Gets list of animals adjacent to a tile. Uses AnimalEnum + 1
+            % as index. Ex: numBears = animalList(AnimalEnum.Bear + 1);
+
+            neighborChange = int8([1,0,-1; 0,1,-1; -1,1,0]);
+            neighborChange = [neighborChange; -1.*neighborChange];
+
+            % Array of whether we have found a particular animal
+            animalsFound = zeros(1, AnimalEnum.NumAnimals);
+
+            for j = 1:size(neighborChange,1)
+                % Get neighbor tile
+                neighborCoords = tile.Coordinate + neighborChange(j,:);
+                neighborTile = tileAtCoords(environment, neighborCoords);
+                if ~isempty(neighborTile.Animal)
+                    animalsFound(neighborTile.Animal + 1) = 1;
+                end
+            end
+        end
+
     end
 end
 
