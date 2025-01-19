7e1b9ed1874c3ddc01fc08f16f93e7ea5c1d2ad5
Tyler Mahlmann <tj@mahlmann.me>
2025-01-18 16:01:12 -0800

    Changed salmon run classification behavior to match Randy's remarks on the topic

--- Game Core/Scoring/SalmonRules.m
+++ Game Core/Scoring/SalmonRules.m
@@ -1,18 +1,18 @@
 classdef SalmonRules < WildlifeScoreObjective
-% IMPORTANT NOTE - There are 2 interpretations for how you can score
-% salmon. The first is following the rulebook: 'Each run of salmon may not
-% have any other salmon adjacent to it'. This means if a run is malformed,
-% you get 0 points for the entire group. However, in the following posts
-% Randy Flynn argues that Salmon should be scored differently:
-%
-% https://boardgamegeek.com/thread/2512694/clarification-on-scoring-salmon-run
-% https://boardgamegeek.com/thread/3087149/salmon-scoring-with-1-wrongly-placed-salmon
-% https://boardgamegeek.com/thread/2809804/tiles-and-salmon-scoring
-% https://boardgamegeek.com/thread/2774858/salmon-card-a-clarification
-%
-% His online version also follows this more forgiving scoring-- a salmon
-% group that isn't wholly a run can have violating tokens removed, and smaller
-% runs scored individually. I choose to follow Randy's approach here.
+    % IMPORTANT NOTE - There are 2 interpretations for how you can score
+    % salmon. The first is following the rulebook: 'Each run of salmon may not
+    % have any other salmon adjacent to it'. This means if a run is malformed,
+    % you get 0 points for the entire group. However, in the following posts
+    % Randy Flynn posits that Salmon should be scored differently:
+    %
+    % https://boardgamegeek.com/thread/2512694/clarification-on-scoring-salmon-run
+    % https://boardgamegeek.com/thread/3087149/salmon-scoring-with-1-wrongly-placed-salmon
+    % https://boardgamegeek.com/thread/2809804/tiles-and-salmon-scoring
+    % https://boardgamegeek.com/thread/2774858/salmon-card-a-clarification
+    %
+    % His online version also follows this more forgiving scoring-- a salmon
+    % group that isn't wholly a run can have violating tokens removed, and smaller
+    % runs scored individually. I choose to follow Randy's approach here.
 
     properties
 
@@ -23,33 +23,24 @@ classdef SalmonRules < WildlifeScoreObjective
             obj.Animal = AnimalEnum.Salmon;
         end
 
-        function groupScore = ruleAShape(obj, environment, groupCoords)
-            groupScore = 0;
+        function groupScore = ruleAShape(obj, ~, groupCoords)
             scoreTable = [2 5 8 12 16 20 25];
-            if isRun(obj, environment, groupCoords)
-                groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
-            end
+            groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
         end
 
-        function groupScore = ruleBShape(obj, environment, groupCoords)
-            groupScore = 0;
+        function groupScore = ruleBShape(obj, ~, groupCoords)
             scoreTable = [2 4 9 11 17];
-            if isRun(obj, environment, groupCoords)
-                groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
-            end
+            groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
         end
 
-        function groupScore = ruleCShape(obj, environment, groupCoords)
-            groupScore = 0;
+        function groupScore = ruleCShape(obj, ~, groupCoords)
             scoreTable = [0 0 10 12 15];
-            if isRun(obj, environment, groupCoords)
-                groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
-            end
+            groupScore = pointsForAttribute(obj, scoreTable, size(groupCoords,1));
         end
 
         function groupScore = ruleDShape(obj, environment, groupCoords)
             groupScore = 0;
-            if isRun(obj, environment, groupCoords) && (size(groupCoords,1) >= 3)
+            if size(groupCoords,1) >= 3
                 runLength = size(groupCoords,1);
 
                 adjacentAnimalCoords = [];
@@ -60,6 +51,7 @@ classdef SalmonRules < WildlifeScoreObjective
                     for j = 1:length(neighborTiles)
                         neighborTile = neighborTiles(j);
                         if ~isempty(neighborTile.WildlifeToken.Animal) && ...
+                                (neighborTile.WildlifeToken.Animal ~= obj.Animal) && ...
                                 (isempty(adjacentAnimalCoords) || ...
                                 ~ismember(neighborTile.Coordinate, adjacentAnimalCoords, "rows"))
                             adjacentAnimalCoords = [adjacentAnimalCoords; neighborTile.Coordinate];
@@ -74,21 +66,55 @@ classdef SalmonRules < WildlifeScoreObjective
         end
     end
 
-    methods(Access = private)
-        function tf = isRun(obj, environment, groupCoords)
-            % Determines whether a group of animals is a 'run'. Each salmon
-            % must have at most 2 neighbor salmon
-            tf = true;
-            for i = 1:size(groupCoords,1)
-                currTile = tileAtCoords(environment, groupCoords(i,:));
-                animalsFound = getAdjacentAnimals(obj, environment, currTile);
+    methods (Access = protected)
+        function totalPoints = pointsForGroups(obj, env, animal, shapeFunc)
+            % Iterates through all groups in an environment, and returns
+            % the total number of points for a particular rule. 'shapeFunc'
+            % is the name of a function that returns the number of
+            % points a group gets based on its coordinates
+            % Overloading from parent class for Salmon -- first we
+            % eliminate any violating salmon, then count groups
 
-                if animalsFound(obj.Animal + 1) > 2
-                    tf = false;
+            % Remove violating salmon
+            allTiles = [env.StarterHabitatTile, env.HabitatTiles];
+            oldEnv = env; 
+            for i = 1:length(allTiles)
+                currTile = allTiles(i);
+                if ~isempty(currTile.WildlifeToken.Animal) && ...
+                        (currTile.WildlifeToken.Animal == obj.Animal)
+                    animalsFound = getAdjacentAnimals(obj, oldEnv, currTile);
+
+                    if animalsFound(obj.Animal + 1) > 2
+                        if i > length(env.StarterHabitatTile)
+                            env.HabitatTiles(i - length(env.StarterHabitatTile)).WildlifeToken = WildlifeToken();
+                        else
+                            env.StarterHabitatTile(i).WildlifeToken = WildlifeToken();
+                        end
+                    end
+                end
+            end
+
+            allTiles = [env.StarterHabitatTile, env.HabitatTiles];
+            totalPoints = 0; overallCoordList = [];
+            for i = 1:length(allTiles)
+                currTile = allTiles(i);
+
+                if ~isempty(overallCoordList)
+                    tileNotInList = ~ismember(currTile.Coordinate,overallCoordList,"rows");
+                else
+                    tileNotInList = true;
+                end
+
+                if ~isempty(currTile.WildlifeToken.Animal) && ...
+                        (currTile.WildlifeToken.Animal == animal) && tileNotInList
+                    groupCoords = recursiveGetAnimalGroupCoords(env, currTile, animal, currTile.Coordinate);
+
+                    totalPoints = totalPoints + feval(shapeFunc, obj, env, groupCoords);
+
+                    overallCoordList = [overallCoordList; groupCoords];
                 end
             end
         end
     end
 
 end
-
